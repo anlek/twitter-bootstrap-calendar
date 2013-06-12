e69829092dd77d2249c46610e9ec0f65fb1e7f94
Andrew Kalek <andrew.kalek@anlek.com>
2013-06-12 01:30:34 -0400

    View inclusion tweak

--- lib/twitter/bootstrap/calendar/engine.rb
+++ lib/twitter/bootstrap/calendar/engine.rb
@@ -6,8 +6,10 @@ module Twitter
   module Bootstrap
     module Calendar
       class Engine < ::Rails::Engine
-        initializer 'twitter-bootstrap-calendar.setup_helpers' do |app|
+        initializer 'twitter-bootstrap-calendar.view_helpers' do |app|
           ActionView::Base.send :include, BootstrapCalendarHelper
+        end
+        initializer 'twitter-bootstrap-calendar.setup_helpers' do |app|
           app.config.to_prepare do
             ActionController::Base.send :helper, BootstrapCalendarHelper
           end
