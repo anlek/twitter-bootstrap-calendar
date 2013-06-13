80745fe1958d40c5564b57f524269c6424e5ef76
Andrew Kalek <andrew.kalek@anlek.com>
2013-06-13 16:28:48 -0400

    Another attempt at including the helper

--- lib/twitter/bootstrap/calendar/engine.rb
+++ lib/twitter/bootstrap/calendar/engine.rb
@@ -6,8 +6,10 @@ module Twitter
   module Bootstrap
     module Calendar
       class Engine < ::Rails::Engine
-        initializer 'twitter-bootstrap-calendar.view_helpers' do |app|
-          ActionView::Base.send :include, BootstrapCalendarHelper
+        initializer 'twitter-bootstrap-calendar.action_controller' do |app|
+          ActiveSupport.on_load :action_controller do
+            helper BootstrapCalendarHelper
+          end
         end
         initializer 'twitter-bootstrap-calendar.setup_helpers' do |app|
           app.config.to_prepare do
